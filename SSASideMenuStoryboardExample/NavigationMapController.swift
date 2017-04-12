f335296227326076cf256773d2401071cd21f9f7
BennyO <opw0011@gmail.com>
2017-04-12 18:06:22 +0800

    Add navigation floor plan web files

--- SSASideMenuStoryboardExample/NavigationMapController.swift
+++ SSASideMenuStoryboardExample/NavigationMapController.swift
@@ -23,10 +23,33 @@ class NavigationMapController: UIViewController, WKUIDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Navigation View Controller loaded")
-        // Do any additional setup after loading the view.
+
+//        let url = URL(string: "https://google.com")!
+//        webView.load(URLRequest(url: url))
         
-        let url = URL(string: "https://google.com")!
-        webView.load(URLRequest(url: url))
+        // load the car park main web view
+        webView.load(URLRequest(url: URL(fileURLWithPath: Bundle.main.path(forResource: "web/index", ofType: "html")!)))
+        
+        // add observer to monitor if the web content complete loading
+        webView.addObserver(self, forKeyPath: "loading", options: .new, context: nil)
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        print("web content is loaded");
+
+        let dataJsonString = "'[{\"bid\": 22, \"rssi\": -59}]'";
+        let javaScriptFunctionString = "updateMarker(0, 0, 'p55', \(dataJsonString))"
+        print(javaScriptFunctionString)
+        self.webView.evaluateJavaScript(javaScriptFunctionString, completionHandler: { (result:Any?, error:Error?) in
+            if let err = error {
+                print(err)
+            }
+        })
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        // unregister the observer
+        webView.removeObserver(self, forKeyPath: "loading", context: nil)
     }
 
     override func didReceiveMemoryWarning() {
