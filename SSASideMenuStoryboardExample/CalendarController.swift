a14cd9e288f53c37c835a9a80674ea64c3833146
BennyO <opw0011@gmail.com>
2017-04-27 14:29:40 +0800

    Upload time schedule to server

--- SSASideMenuStoryboardExample/CalendarController.swift
+++ SSASideMenuStoryboardExample/CalendarController.swift
@@ -13,6 +13,8 @@ import Parse
 
 class CalendarController: DayViewController {
     
+    var myParkingSpaceID: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -43,14 +45,13 @@ class CalendarController: DayViewController {
             }
             
             // parking space found
-
+            self.myParkingSpaceID = parkingSpace?.objectId
             let schedules = parkingSpace!["schedule"] as? [[String: AnyObject]]
             print(schedules ?? "No schedule")
             if let schedules = schedules {
                 // clear previous schedules
                 myParkingSchdules.removeAll()
                 
-            
                 for schedule in schedules {
                     if let weekday = schedule["weekday"] as? Int,
                         let startTimeHour = schedule["startTime"] as? Float,
@@ -62,10 +63,11 @@ class CalendarController: DayViewController {
                     }
                 }
                 
+                // reload calendar view
+                self.dayView.reloadData()
             }
             
-            // reload calendar view
-            self.dayView.reloadData()
+
         }
 
     }
@@ -78,14 +80,37 @@ class CalendarController: DayViewController {
         myParkingSchdules.append(schedule)
         
         // TODO: Upload the updated schedule to Server
+        syncScheduleWithServer()
         
         self.dayView.reloadData()
     }
     
+    func syncScheduleWithServer() -> Void {
+        var requestParam = [String: Any]()
+        var scheduleArray = [Any]()
+        for schedule in myParkingSchdules {
+            scheduleArray.append(schedule.getDictionary())
+        }
+        requestParam["schedule"] = scheduleArray
+        requestParam["parkingSpaceId"] = self.myParkingSpaceID ?? ""
+        
+        print(requestParam)
+        
+        
+        PFCloud.callFunction(inBackground: "release", withParameters: requestParam) { (response:Any?, error:Error?) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let res = response {
+                print(res)
+            }
+        }
+    }
+    
+    
     override func eventsForDate(_ date: Date) -> [EventDescriptor] {
         var events = [Event]()
-        
-        print("Load view of \(myParkingSchdules.count) events")
         
         for schedule in myParkingSchdules {
             //print("Calendar load \(schedule)")
@@ -100,10 +125,13 @@ class CalendarController: DayViewController {
 
             // Set "text" value of event by formatting all the information needed for display
             event.color = UIColor.white
-            event.backgroundColor = UIColor.blue
+            event.backgroundColor = UIColor.init(r: 107, g: 69, b: 186, a: 100)
             event.text = "\(schedule.parkingLotID)\n \(datePeriod.beginning!.format(with: "HH:mm")) - \(datePeriod.end!.format(with: "HH:mm"))"
+            print(event.text)
             events.append(event)
         }
+        
+        print("Load view of \(myParkingSchdules.count) / \(events.count) events")
         
         return events
     }
@@ -136,9 +164,12 @@ class CalendarController: DayViewController {
                     print("Delete \(i)")
                     //myParkingSchdules.removeAll()
                     myParkingSchdules.remove(at: i)
+                    self.reloadInputViews()
+                    self.reloadData()
                     self.dayView.reloadData()
                     self.dayView.reloadInputViews()
                     //eventView.reloadInputViews()
+                    self.syncScheduleWithServer()
                     
                 }
             }
